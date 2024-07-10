bf51485aabe2cf2932749f48fafe521bf3164f23
JannersLSR <johnrobert.formal@gmail.com>
2024-07-11 04:26:46 +0800

    finished all events

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     console.log('Student created successfully');
                 } else {
                     //console.error('Failed to create student:', res.status);
-                    displayErrorMessage('Failed to create student or Student already exists.', 'error-message');
+                    displayErrorMessage('Student already exists or Missing parameters.', 'error-message');
                 }
             } catch (error) {
                 //console.error('Error creating student:', error);
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     console.log('Student updated successfully:', data);
                 } else {
                     console.error('Failed to update student:', res.status);
-                    displayErrorMessage('Failed to update student. Please try again.', 'error-message');
+                    displayErrorMessage('Missing parameters. Please try again.', 'error-message');
                 }
             } catch (error) {
                 //console.error('Error updating student:', error);
@@ -126,7 +126,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     console.log('Student deleted successfully');
                 } else {
                     console.error('Failed to delete student:', res.status);
-                    displayErrorMessage('Failed to delete student/Student does not exist. Please try again.', 'error-message');
+                    displayErrorMessage('Student does not exist.', 'error-message');
                 }
             } catch (error) {
                 console.error('Error deleting student:', error);
@@ -137,6 +137,151 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Delete student button not found.');
     }
 });
+
+// CREATE EVENT
+document.addEventListener('DOMContentLoaded', () => {
+    const createEventButton = document.getElementById('create-event');
+    if (createEventButton) {
+        createEventButton.addEventListener('click', async function(event){
+            const eventNum = document.getElementById('eventNum').value;
+            const eventName = document.getElementById('eventName').value;
+            const eventStart = document.getElementById('eventStart').value;
+            const eventEnd = document.getElementById('eventEnd').value;
+            const eventDate = document.getElementById('eventDate').value;
+
+            try {
+                const res = await fetch('http://localhost:3000/create/events', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ eventNum, eventName, eventStart, eventEnd, eventDate })
+                });
+
+                if (res.ok) {
+                    console.log('Event created successfully');
+                } else {
+                    //console.error('Failed to create student:', res.status);
+                    displayErrorMessage('Event already exists or Missing parameters.', 'error-message');
+                }
+            } catch (error) {
+                //console.error('Error creating student:', error);
+                displayErrorMessage('Error creating event. Please try again.', 'error-message');
+            }
+        });
+    } else {
+        console.error('Create event button not found.');
+    }
+});
+
+
+// QUERY EVENT
+document.addEventListener('DOMContentLoaded', () => {
+    const queryEventButton = document.getElementById('query-event');
+    if (queryEventButton) {
+        queryEventButton.addEventListener('click', async function(event){
+            const eventNumber = document.getElementById('eventNum').value;
+
+            try {
+                const res = await fetch('http://localhost:3000/query/events', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ eventNumber })
+                });
+
+                if (res.ok) {
+                    const data = await res.json();
+
+                    clearErrorMessage();
+
+                    document.getElementById('q-ename').textContent = data.eventName;
+                    document.getElementById('q-estart').textContent = data.eventStart;
+                    document.getElementById('q-eend').textContent = data.eventEnd;
+                    document.getElementById('q-edate').textContent = data.eventDate;
+                } else {
+                    console.error('Failed to fetch event:', res.status);
+                    displayErrorMessage('Event Number not Found!');
+                }
+            } catch (error) {
+                console.error('Error fetching event:', error);
+            }
+        });
+    } else {
+        console.error('Currently not in correct page');
+    }
+});
+
+// UPDATE EVENT
+document.addEventListener('DOMContentLoaded', () => {
+    const updateEventButton = document.getElementById('update-event');
+    if (updateEventButton) {
+        updateEventButton.addEventListener('click', async function(event){
+            const eventNum = document.getElementById('eventNum').value;
+            const eventName = document.getElementById('eventName').value;
+            const eventStart = document.getElementById('eventStart').value;
+            const eventEnd = document.getElementById('eventEnd').value;
+            const eventDate = document.getElementById('eventDate').value;
+
+            try {
+                const res = await fetch('http://localhost:3000/update/events', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ eventNum, eventName, eventStart, eventEnd, eventDate })
+                });
+
+                if (res.ok) {
+                    const data = await res.json();
+                    console.log('Event updated successfully:', data);
+                } else {
+                    console.error('Failed to update event:', res.status);
+                    displayErrorMessage('Missing parameters. Please try again.', 'error-message');
+                }
+            } catch (error) {
+                //console.error('Error updating student:', error);
+                
+            }
+        });
+    } else {
+        console.error('Update event button not found.');
+    }
+});
+
+// DELETE EVENTS
+document.addEventListener('DOMContentLoaded', () => {
+    const deleteEventButton = document.getElementById('delete-event');
+    if (deleteEventButton) {
+        deleteEventButton.addEventListener('click', async function(event){
+            const eventNumber = document.getElementById('eventNum').value;
+
+            try {
+                const res = await fetch('http://localhost:3000/delete/events', {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ eventNum: eventNumber })
+                });
+
+                if (res.ok) {
+                    console.log('Event deleted successfully');
+                } else {
+                    console.error('Failed to delete event:', res.status);
+                    displayErrorMessage('Event does not exist.', 'error-message');
+                }
+            } catch (error) {
+                console.error('Error deleting event:', error);
+                displayErrorMessage('Error deleting event. Please try again.', 'error-message');
+            }
+        });
+    } else {
+        console.error('Delete event button not found.');
+    }
+});
+
 
 // LOGIN USER
 document.addEventListener('DOMContentLoaded', () => {
@@ -170,6 +315,10 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Login button not found.');
     }
 });
+
+
+
+
 
 function displayErrorMessage(message) {
 
--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/create/students', async (req, res) => {
     }
 });
 
-// QUERY-UPDATE STUDENT
+// QUERY STUDENT
 app.post('/query/students', async (req, res) => {
     const { studNumber } = req.body;
     let connection;
@@ -58,7 +58,7 @@ app.post('/query/students', async (req, res) => {
         });
 
         const result = await connection.execute(
-            `SELECT studfname, studlname, studhouse FROM Students WHERE studID = :studNum`,
+            `SELECT studfname, studlname, studhouse FROM Students WHERE studID = :studNumber`,
             [studNumber]
         );
 
@@ -158,6 +158,155 @@ app.delete('/delete/students', async (req, res) => {
     }
 });
 
+
+// CREATE EVENT
+app.post('/create/events', async (req, res) => {
+    const { eventNum, eventName, eventStart, eventEnd, eventDate } = req.body;
+    let connection;
+    try {
+        connection = await oracledb.getConnection({
+            user: 'system',
+            password: 'admin',
+            connectString: 'localhost/XE'
+        });
+
+        const existingResult = await connection.execute(`SELECT COUNT(*) AS count FROM Events WHERE eventID = :eventNum`, 
+            [eventNum]);
+
+        if (existingResult.rows[0][0] > 0) {
+            res.status(400).send('Event ID already exists');
+        } else {
+            await connection.execute(`INSERT INTO Events (eventID, eventName, eventStart, eventEnd, eventDate) VALUES (:eventNum, :eventName, :eventStart, :eventEnd, :eventDate)`, 
+                [ eventNum, eventName, eventStart, eventEnd, eventDate ]);
+            await connection.commit();
+            res.status(200).send('Event created successfully');
+        }
+    } catch (err) {
+        res.status(500).send('An error occurred. Please try again later.');
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+});
+
+// QUERY EVENT
+app.post('/query/events', async (req, res) => {
+    const { eventNumber } = req.body;
+    let connection;
+    try {
+        connection = await oracledb.getConnection({
+            user: 'system',
+            password: 'admin',
+            connectString: 'localhost/XE'
+        });
+
+        const result = await connection.execute(
+            `SELECT eventName, eventStart, eventEnd, eventDate FROM Events WHERE eventID = :eventNumber`,
+            [eventNumber]
+        );
+
+        if (result.rows.length > 0) {
+            const eventData = {
+                eventName: result.rows[0][0],
+                eventStart: result.rows[0][1],
+                eventEnd: result.rows[0][2],
+                eventDate: result.rows[0][3]
+            };
+            res.status(200).json(eventData);
+        } else {
+            res.status(404).send('Event not found');
+        }
+    } catch (err) {
+        res.status(500).send('An error occurred. Please try again later.');
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+});
+
+// UPDATE EVENT
+app.post('/update/events', async (req, res) => {
+    const { eventName, eventStart, eventEnd, eventDate, eventNum } = req.body;
+    let connection;
+    try {
+        connection = await oracledb.getConnection({
+            user: 'system',
+            password: 'admin',
+            connectString: 'localhost/XE'
+        });
+
+        const result = await connection.execute(
+            `UPDATE Events SET eventName = :eventName, eventStart = :eventStart, eventEnd = :eventEnd, eventDate = :eventDate WHERE eventID = :eventNum`,
+            { eventName, eventStart, eventEnd, eventDate, eventNum },
+            {autoCommit: true}
+        );
+
+        if (result.rowsAffected && result.rowsAffected === 1) {
+            res.status(200).send('Event updated successfully');
+        } else {
+            res.status(404).send('Event not found or update failed');
+        }
+    } catch (err) {
+        res.status(500).send('An error occurred. Please try again later.');
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+});
+
+// DELETE EVENT
+app.delete('/delete/events', async (req, res) => {
+    const { eventNum } = req.body;
+    let connection;
+    try {
+        connection = await oracledb.getConnection({
+            user: 'system',
+            password: 'admin',
+            connectString: 'localhost/XE'
+        });
+
+        const result = await connection.execute(
+            `DELETE FROM Events WHERE eventID = :eventNum`,
+            [eventNum],
+            {autoCommit: true}
+        );
+
+        if (result.rowsAffected && result.rowsAffected === 1) {
+            res.status(200).send('Event deleted successfully');
+        } else {
+            res.status(404).send('Event not found or delete failed');
+        }
+    } catch (err) {
+        res.status(500).send('An error occurred. Please try again later.');
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+});
 
 // USER LOGIN
 app.post('/query/users', async (req, res) => {
